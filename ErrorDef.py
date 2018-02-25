e28f3ef13f3d3007c2875a853fef538146caab0f
asassmann <36732360+asassmann@users.noreply.github.com>
2018-02-25 23:19:23 +0100

    Updated with SVACOMP Error Def

--- ErrorDef.py
+++ ErrorDef.py
@@ -41,5 +41,34 @@ class ErrorDef:
             if detText in ['a','an','the','']:
                 return [probStart,probEnd,syntval,self.ErrorType]
         return 0
-        
+    
+    def checkSVACOMP(self, subj, subjind, verb, verbind, syntval, sentDet):
+        if str(p.singular_noun(subj)) == "False":#subj is singular
+            print("singular")
+            plural_verb = p.plural_verb(verb)
+            if verb != plural_verb:
+                return 0
+                #print("good")
+            else:
+                print("SVA problem found")
+                return [subjind,verbind,syntval,self.ErrorType]
+                
+        else:
+            print("plural")
+            plural_verb = p.plural_verb(verb)
+            if verb == plural_verb:
+                return 0
+                #print("good")
+            else:
+                print("SVA problem found")
+                return [subjind,verbind,syntval,self.ErrorType]
+                #print("bad")
+    
+    def checkSVACOMPplural(self, verb, verbind, syntval, sentDet):
+        plural_verb = p.plural_verb(verb)
+        #print(plural_verb)
+        if verb == plural_verb:
+            return 0
+        else:
+            return [verbind,verbind,syntval,self.ErrorType]
     
