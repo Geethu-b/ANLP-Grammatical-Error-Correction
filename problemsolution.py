07932a2ce74c296a8f84219772c0c52497b23a56
asassmann <astrid@ra2u.de>
2018-03-03 16:41:07 +0100

    Added Comments
    Signed-off-by: asassmann <astrid@ra2u.de>

--- problemsolution.py
+++ problemsolution.py
@@ -2,7 +2,40 @@ import GoogleSentList as googSentList
 import checkGoogle as cg
 from word_forms.word_forms import get_word_forms
 
-
+#Class containing sentences and their details.
+#Its attributes are:
+#    start					the start of the problem
+#    end					the end of the problem
+#    size					the size of the problem
+#    typeProb				a parameter containing additional information depending on the errorType
+#    errorType				a string specifying the type of the error
+#    lstWords				a list containing all words of a sentence
+#    lstChecksentoptions	a list of checked sentences after adding required logics for ART or Spel, others type of error types 
+#    allowLeft				number of words left to the problem can be used for evaluation
+#    allowRight				number of words right to the problem can be used for evaluation
+#    chkMarked				boolean that is true for Spelling and Article Determinator Errors
+#    chkWord				the word if only a single word needs to be checked
+#    currentWord			the word currently evaluated
+#    optionList				a list of suggestions from SPEL Checking errorType
+#    solution				the solution of the problem
+#	 bestOpt				a string specifying the best option on how to solve the problem
+#    bestOptval				the value of the best solution, i.e. the word
+#	 bestScore				the best score of an ngram
+#
+#Its methods are:
+#    __init__				the initialization of a ProblemSolution object
+#    getSVASolution			gets the solution of Subject Verb Agreement Errors
+#    getCorrectVerb			generates the correct verb form
+#    to_plural				gives back the plural form of a verb
+#    to_3th_person			gives back the 3rd person singular present of a verb
+#    getOptionlist			takes the word and the option and then changes to that required category
+#    getSpelSolution		gets the correct spelling for a incorrect spelled word
+#    getOthersolution		gets other solutions
+#    genChecklist			checks what boundaries an ngram containing the problem may have
+#    reChkRedSize			tries different bounderies for the ngram of the problem to get scores different from zero for the possible solution
+#    getNgramcount			gets the ngram count for a possible solution with Google ngram
+#    scoreCheck				checks if the best score of the ngram is 0 to see if it is applicable as solution
+#    getSolution			returns the solution
 class ProblemSolution:
     start               = 0
     end                 = 0
@@ -24,6 +57,13 @@ class ProblemSolution:
     # the text will be blank
     solution            = None
     
+	#Initializes the ProblemSolution object
+	#input:
+	#	startval		the start index of the problem
+	#	endval			the end index of the problem
+	#	typep			a parameter containing additional information depending on the errorType
+	#	errorTypeval	the type of the error as a string
+	#	lstWordval		a list containing all the words of the sentence
     def __init__(self,startval,endval,typep,errorTypeval,lstWordval):
         self.start               = int(startval)
         self.end                 = int(endval)
@@ -55,14 +95,19 @@ class ProblemSolution:
         if self.errorType in ['SVACOMP','SVACOMPplural']:
             self.getSVASolution()
             
-
+	#Gets the solution of Subject Verb Agreement Errors by replacing the incorrect form with the correct form
     def getSVASolution(self):
         startPos        = self.end
         self.bestOpt    = "REP"
         self.bestOptval = self.getCorrectVerb(self.lstWords[startPos],self.typeProb)
         self.solution   = [startPos,self.bestOpt,self.bestOptval]
         
-        
+    #Generates the correct verb form depending on the parameter option
+	#input:
+	#	verb	the incorrect verb
+	#	option	a string containing information about the correct form of the verb
+	#output:
+	#	retval	the corrected verb
     def getCorrectVerb(self,verb, option):
         retval = ""
         if option == 'TO_PLURAL':
@@ -76,7 +121,12 @@ class ProblemSolution:
         
         return retval
             
-
+	#Gives back the plural form of a verb by handwriten rules
+	#input:
+	#	verb	the verb in its singular form
+	#output:
+	#	""		a string of the plural form of the verb
+	#	verb	the verb itself it doesn't fit the rules
     def to_plural(self,verb):
         if verb == "is":
             return "are"
@@ -92,7 +142,12 @@ class ProblemSolution:
         if verb[-1] == 's':
             return verb[:-1]
         return verb
-    
+
+	#Gives back the 3rd person singular present of a verb by handwriten rules
+	#input:
+	#	verb	the verb in its other form
+	#output:
+	#	""		a string of the wnated form of the verb
     def to_3th_person(self,verb):
         if verb == 'are':
             return 'is'
@@ -110,12 +165,16 @@ class ProblemSolution:
 
         return verb + 's'
 
-
+	#The function takes the word and the option and then changes to that required category. 
+	#input:
+	#	checkWord	 Input word 
+	#	optionPass	 The required tag to be converted
+	#output:
+	#	list		it return a required value after changing to another type 
     def getOptionlist(self,checkWord,optionPass):   
         checkWord = checkWord.lower()
         outdic ={}
         outdic = get_word_forms(checkWord)
-        #print(outdic)
         
         if optionPass in ['NNS','NN','NNP','NNPS']:
             return list(outdic.get('n'))
@@ -126,6 +185,7 @@ class ProblemSolution:
         elif optionPass in ['VB','VBD','VBG','VBN','VBZ']:
             return list(outdic.get('v'))
         
+	#Gets the correct spelling for a incorrect spelled word by checking with the Google ngrams for the context in the sentence
     def getSpelSolution(self):
         mleft  = 1
         mRight = 1
@@ -142,8 +202,8 @@ class ProblemSolution:
         self.optionList =self.typeProb
         
         self.lstChecksentoptions = genSentence.genSentlistOther(self.allowLeft,self.allowRight,self.typeProb,self.currentWord,self.optionList)
-        #print(self.lstChecksentoptions)
 
+	#Gets other solutions by checking with the Google ngrams for the context in the sentence
     def getOthersolution(self):
         mleft  = 1
         mRight = 1
@@ -160,9 +220,8 @@ class ProblemSolution:
         self.optionList = self.getOptionlist(self.currentWord,self.typeProb)
         
         self.lstChecksentoptions = genSentence.genSentlistOther(self.allowLeft,self.allowRight,self.typeProb,self.currentWord,self.optionList)
-        #print(self.lstChecksentoptions)
         
-
+	#Checks what boundaries an ngram containing the problem may have
     def genChecklist(self):        
         
         self.problemCondition = True
@@ -199,7 +258,10 @@ class ProblemSolution:
             self.solution=[self.start,"NA",""]
                 
             
-        
+	#Tries different bounderies for the ngram of the problem to get scores different from zero for the possible solution
+	#output:
+	#	1		if there is a score higher than zero
+	#	retval	has a value of 0 since no higher score has been found
     def reChkRedSize(self):
         retVal = 0 
         if self.size==3:                    
@@ -275,13 +337,13 @@ class ProblemSolution:
 
         return retVal
         
+	#Gets the ngram count for a possible solution with Google ngram
     def getNgramcount(self):
         self.bestScore  = 0
         self.bestOpt    = ""
         self.bestOptval = ""
         # get the best value to determine the solution
         for sent,opt,optval in self.lstChecksentoptions:
-            print(sent,opt,optval)
             try:
                 outVal = cg.qryGoogle(sent)
                 #setting threshold to 1000
@@ -290,8 +352,6 @@ class ProblemSolution:
                 #giving a extra score for ArtChk Checked Word
                 if outVal !=0 and optval==self.chkWord and self.errorType in ['ArtChk','SPEL']:
                     outVal +=20000
-                print(sent,opt,optval,self.chkWord)
-                print(outVal)
                 
                 if outVal>self.bestScore:
                     self.bestScore  = outVal
@@ -302,6 +362,9 @@ class ProblemSolution:
                 self.lstChecksentoptions.append([sent,opt,optval])
         #if no best score is found the solution is not applicable
      
+	#If the best score of the ngrams is not zero, it will be the solution
+	#If not, there is no solution except if it is an ArtChk error
+	#Then it depends on the best option
     def scoreCheck(self):   
         if self.bestScore ==0:
             if self.reChkRedSize() == 0:
@@ -319,5 +382,9 @@ class ProblemSolution:
                 self.solution = [self.start,'NA','']
             else:
                 self.solution = [self.start,'REP',self.bestOptval]
+				
+	#Returns the solution
+	#output:
+	#	self.solution	the solution
     def getSolution(self):
         return self.solution
